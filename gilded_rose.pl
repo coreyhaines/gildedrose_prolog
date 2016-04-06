fa2989574a2761d1770d49cc9d7807fdccc5c52d
Corey Haines <coreyhaines@gmail.com>
2016-04-06 07:54:19 -0500

    Rather than creating a fact each time a day passes, just pass in the number of days that have passed and calculate that way.
    This feels a bit better, as we aren't temporarily-coupled

--- gilded_rose.pl
+++ gilded_rose.pl
@@ -6,46 +6,27 @@ item(brie, 10, 2).
 
 improvingItem(brie).
 
-availableProducts(Name, Quality) :-
-  itemQuality(Name, Quality).
+products(Name, Quality, DaysPassed) :-
+  itemQuality(Name, Quality, DaysPassed).
 
 guardQuality(Quality, GuardedQuality) :-
   Quality > 50, GuardedQuality is 50;
   Quality < 0, GuardedQuality is 0;
   Quality >= 0, Quality =< 50, GuardedQuality is Quality.
 
-itemQuality(Name, Quality) :-
- item(Name, OriginalQuality, SellIn),
+itemQuality(Name, Quality, DaysPassed) :-
+ item(Name, OriginalQuality, _),
  improvingItem(Name),
- daysPassed(DaysPassed),
- DaysPassed =< SellIn,
  guardQuality(OriginalQuality + DaysPassed, Quality).
 
-itemQuality(Name, Quality) :-
+itemQuality(Name, Quality, DaysPassed) :-
   item(Name, OriginalQuality, SellIn),
   not(improvingItem(Name)),
-  daysPassed(DaysPassed),
   DaysPassed =< SellIn,
   guardQuality(OriginalQuality - DaysPassed, Quality).
 
-itemQuality(Name, Quality) :-
+itemQuality(Name, Quality, DaysPassed) :-
   item(Name, OriginalQuality, SellIn),
   not(improvingItem(Name)),
-  daysPassed(DaysPassed),
   DaysPassed > SellIn,
   guardQuality(OriginalQuality - SellIn - (2*(DaysPassed-SellIn)), Quality).
-
-:- dynamic day_passed/1.
-day_has_passed() :-
-  asserta(day_passed(1)).
-
-sum(List, Sum) :-
-  sumRec(List, 0, Sum).
-sumRec([], Value, Value).
-sumRec([H|T], Value, Accum) :-
-  sumRec(T, Value, OldAccum),
-  Accum is OldAccum + H.
-
-daysPassed(Count) :-
-  findall(Number, day_passed(Number), DaysPassed),
-  sum(DaysPassed, Count).
