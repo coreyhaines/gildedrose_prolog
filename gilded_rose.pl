f238d85ea6ac8dbc0b0943454c0a910d0f33c240
Corey Haines <coreyhaines@gmail.com>
2016-04-06 08:24:04 -0500

    Add legendary items

--- gilded_rose.pl
+++ gilded_rose.pl
@@ -3,8 +3,10 @@ item(apple, 10, 1).
 item(pear, 3, 5).
 item(peach, 1, 1).
 item(brie, 10, 2).
+item(sulfras, 10, 0).
 
 improvingItem(brie).
+legendaryItem(sulfras).
 
 products(Name, Quality, DaysPassed) :-
   itemQuality(Name, Quality, DaysPassed).
@@ -14,6 +16,11 @@ guardQuality(Quality, GuardedQuality) :-
   Quality < 0, GuardedQuality is 0;
   Quality >= 0, Quality =< 50, GuardedQuality is Quality.
 
+itemQuality(Name, Quality, _) :-
+ item(Name, OriginalQuality, _),
+ legendaryItem(Name),
+  Quality is OriginalQuality.
+
 itemQuality(Name, Quality, DaysPassed) :-
  item(Name, OriginalQuality, _),
  improvingItem(Name),
@@ -22,11 +29,13 @@ itemQuality(Name, Quality, DaysPassed) :-
 itemQuality(Name, Quality, DaysPassed) :-
   item(Name, OriginalQuality, SellIn),
   not(improvingItem(Name)),
+  not(legendaryItem(Name)),
   DaysPassed =< SellIn,
   guardQuality(OriginalQuality - DaysPassed, Quality).
 
 itemQuality(Name, Quality, DaysPassed) :-
   item(Name, OriginalQuality, SellIn),
   not(improvingItem(Name)),
+  not(legendaryItem(Name)),
   DaysPassed > SellIn,
   guardQuality(OriginalQuality - SellIn - (2*(DaysPassed-SellIn)), Quality).
